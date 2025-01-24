f7c8011006ce73a5d02cde9c172e3a4939ea5b23
brajankukk <brkukk@taltech.ee>
2025-01-24 23:41:38 +0200

    added domain

---
+++ Back-end/App.Domain/BaseEntity.cs
@@ -0,0 +1,12 @@
+using Base.Contracts.Domain;
+
+namespace App.Domain;
+
+public abstract class BaseEntity : IDomainEntityId, IDomainEntityMetadata
+{
+    public Guid Id { get; set; } = default!;
+    public string CreatedBy { get; set; } = default!;
+    public DateTime CreatedAt { get; set; } = default!;
+    public string UpdatedBy { get; set; } = default!;
+    public DateTime UpdatedAt { get; set; } = default!;
+}
--- Back-end/App.Domain/ToDo.cs
+++ Back-end/App.Domain/ToDo.cs
@@ -1,6 +1,18 @@
+
 namespace App.Domain;
 
-public class ToDo
+public class ToDo : BaseEntity
 {
+    public string Title { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public bool Completed { get; set; } = default!;
+    
+    public Guid? parentTaskId { get; set; }
+    
+    public ToDo? parentTask { get; set; }
+    
+    public ICollection<ToDo>? subTasks { get; set; }
     
 }
