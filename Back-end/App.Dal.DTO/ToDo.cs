a10d8c853985738e3e6553a4da0d0df46c2cc240
brajankukk <brkukk@taltech.ee>
2025-01-25 00:17:13 +0200

    finishing Dto class

--- Back-end/App.Dal.DTO/ToDo.cs
+++ Back-end/App.Dal.DTO/ToDo.cs
@@ -1,6 +1,16 @@
 namespace App.Dal.DTO;
 
-public class ToDo
+public class ToDo: BaseEntity
 {
+    public string Title { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public bool Completed { get; set; } = default!;
     
+    public Guid? parentTaskId { get; set; }
+    
+    public ToDo? parentTask { get; set; }
+    
+    public ICollection<ToDo>? subTasks { get; set; }
 }
