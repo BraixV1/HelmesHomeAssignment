6e4560347be6cb6b33ffe4c84af65019f1d0a848
brajankukk <brkukk@taltech.ee>
2025-01-25 00:25:30 +0200

    making automapper clearer

--- Back-end/App.Dal.EF/AutoMapperProfile.cs
+++ Back-end/App.Dal.EF/AutoMapperProfile.cs
@@ -6,6 +6,6 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<App.Domain.ToDo, App.Dal.DTO.ToDo>().ReverseMap();
+        CreateMap<Domain.ToDo, DTO.ToDo>().ReverseMap();
     }
 }
