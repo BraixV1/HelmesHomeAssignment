d78f423b35632766a7fa0ad1eee908ceecb93832
brajankukk <brkukk@taltech.ee>
2025-01-25 01:04:17 +0200

    adding Bll DTO

---
+++ Back-end/App.BLL.DTO/ToDo.cs
@@ -0,0 +1,16 @@
+namespace App.BLL.DTO;
+
+public class ToDo : BaseEntity
+{
+    public string Title { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public bool Completed { get; set; } = default!;
+    
+    public Guid? parentTaskId { get; set; }
+    
+    public ToDo? parentTask { get; set; }
+    
+    public ICollection<ToDo>? subTasks { get; set; }
+}
