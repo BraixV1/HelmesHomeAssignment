dc3b5b04a45fbbf72ca5ae8690323f1cfcec37f5
brajankukk <brkukk@taltech.ee>
2025-01-25 15:11:47 +0200

    addubg filter Params

---
+++ Back-end/App.Resources/FilterParams.cs
@@ -0,0 +1,10 @@
+namespace App.Resources;
+
+public class FilterParams
+{
+    public string? NameFilter { get; set; }
+    public string? DescFilter { get; set; }
+    public bool? DoneFilter { get; set; }
+    public DateTime? DueDateTime { get; set; }
+    
+}
