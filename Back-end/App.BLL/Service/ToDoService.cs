d78b3f25b3950f6de76514be3f3791fc7bcb7795
brajankukk <brkukk@taltech.ee>
2025-01-25 15:14:01 +0200

    IMplementing GetAllPaginated to servce

--- Back-end/App.BLL/Service/ToDoService.cs
+++ Back-end/App.BLL/Service/ToDoService.cs
@@ -1,6 +1,7 @@
 using App.Contracts.BLL.Services;
 using App.Contracts.DAL.Repositories;
 using App.Dal.DTO;
+using App.Resources;
 using AutoMapper;
 using Base.BLL;
 using Base.Contracts.DAL;
@@ -9,5 +10,25 @@ namespace App.BLL.Service;
 
 public class ToDoService : BaseEntityService<App.Dal.DTO.ToDo, App.BLL.DTO.ToDo, IToDoRepository>, IToDoService
 {
-    public ToDoService(IUnitOfWork uoW, IToDoRepository repository, IMapper mapper) : base(uoW, repository, new BllDalMapper<ToDo, App.BLL.DTO.ToDo>(mapper)) {}
+
+    private readonly IMapper _mapper;
+    public ToDoService(IUnitOfWork uoW, IToDoRepository repository, IMapper mapper) : base(uoW, repository,
+        new BllDalMapper<ToDo, App.BLL.DTO.ToDo>(mapper))
+    {
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+    public async Task<PaginatedResponse<DTO.ToDo>> GetAllFiltered(FilterParams filterParams, int pageNumber, int pageSize)
+    {
+        var resultCall = await Repository.GetAllFiltered(filterParams, pageNumber, pageSize);
+
+        var result = new PaginatedResponse<DTO.ToDo>
+        {
+            Items = resultCall.Items.Select(toDo => _mapper.Map<DTO.ToDo>(toDo)),
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = resultCall.TotalCount
+        };
+
+        return result;
+    }
 }
